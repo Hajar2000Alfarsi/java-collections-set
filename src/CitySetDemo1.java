d381537b786aad05d3956a151a6c0dd1a2f4ad00
Hajar <hajar2000alfarsi@gmail.com>
2026-04-15 13:01:14 +0400

    Add Task 18: Store and Manage Unique City Names in a Java Set

---
+++ src/CitySetDemo1.java
@@ -0,0 +1,55 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class CitySetDemo1 {
+    public static void main(String[] args){
+        Set<String> cityNames= new HashSet<>();
+        cityNames.add("Muscat");
+        cityNames.add("Salalah");
+        cityNames.add("Sohar");
+        cityNames.add("Nizwa");
+        cityNames.add("Sur");
+
+        System.out.println("City Names: ");
+        for (String names:cityNames){
+            System.out.println(names);
+        }
+
+        cityNames.add("Ibri");
+        System.out.println("City Names(After Add city Ibri): ");
+        for (String names:cityNames){
+            System.out.println(names);
+        }
+        cityNames.add("Muscat");
+        System.out.println("City Names(After Add city Muscat): ");
+        for (String names:cityNames){
+            System.out.println(names);
+        }
+
+        boolean exists=cityNames.contains("Sur");
+        System.out.println("Does Sur exist? " + exists);
+        boolean exists1=cityNames.contains("Barka");
+        System.out.println("Does Barka exist? " + exists1);
+
+        cityNames.remove("Muscat");
+        System.out.println("City Names(After removing city Muscat): ");
+        for (String names:cityNames){
+            System.out.println(names);
+        }
+        System.out.println("Total number of Cites: " + cityNames.size());
+
+        cityNames.add("Bahla");
+        System.out.println("City Names(After Add city Bahla): ");
+        for (String names:cityNames) {
+            System.out.println(names);
+        }
+        System.out.println("Total number of Cites: " + cityNames.size());
+
+        cityNames.clear();
+        System.out.println("City Names(After removing all Cites): ");
+        for (String names:cityNames){
+            System.out.println(names);
+        }
+        System.out.println("Total number of Cites: " + cityNames.size());
+    }
+}
